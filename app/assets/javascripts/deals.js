8012ee73ec6f17b0fa420688fc4d723cb326f6dd
jplethier <jplethier@gmail.com>
2012-04-21 15:59:26 -0300

    removendo o commit inicial

--- app/assets/javascripts/deals.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $('#search_order').change(function() {
-    $('#search_form').submit();
-  });
-  $('#search_city').change(function() {
-    $('#search_form').submit();
-  });
-});
--- app/controllers/authorized_controller.rb
+++
@@ -1,5 +0,0 @@
-class AuthorizedController < ApplicationController
-  before_filter :authenticate_user!
-  check_authorization
-  load_and_authorize_resource
-end

7217ef31f92a6b0a2463237dfdd8822855dbb4d7
jplethier <jplethier@gmail.com>
2012-04-30 19:53:22 -0300

    criando pagina publica pra ver se resolve o problema do blitz do claoudflare

--- config/routes.rb
+++ config/routes.rb
@@ -37,7 +37,7 @@ DealWitMe::Application.routes.draw do
     end
   end
 
-  match '/mu-4b4c57f5-b0d80846-731c8e63-0f3bcb00', :to => "pages#blitz"
+  # match '/mu-4b4c57f5-b0d80846-731c8e63-0f3bcb00', :to => "pages#blitz"
 
   match '/deals/today/:category', :to => "deals#today"
   match '/deals/active/:category', :to => "deals#index"

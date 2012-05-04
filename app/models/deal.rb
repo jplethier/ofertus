9540ab61bc6c34bc171345e6d59b6e90ebe2091e
jplethier <jplethier@gmail.com>
2012-05-03 21:39:28 -0300

    tentando consertar a data de validade da oferta

--- app/models/deal.rb
+++ app/models/deal.rb
@@ -72,8 +72,8 @@ class Deal < ActiveRecord::Base
   scope :best_deals, order("(deals.up_votes / (deals.up_votes + deals.down_votes)) DESC")
   scope :most_commented, order("(select count(comments.id) from comments where comments.commentable_id = deals.id) DESC")
 
-  scope :today, where("deals.created_at >= ?", Time.zone.now.to_date)
-  scope :active, where("deals.end_date >= ?", Time.zone.now.to_date)
+  scope :today, where("deals.created_at >= ?", Time.zone.today)
+  scope :active, where("deals.end_date >= ?", Time.zone.today)
   scope :voted, where("(deals.up_votes + deals.down_votes) > 0")
 
 
@@ -147,8 +147,8 @@ class Deal < ActiveRecord::Base
 
   def set_default_date
     puts "-"*500
-    puts Time.zone.now.to_date
-    self.end_date = Time.zone.now.to_date if self.end_date.nil?
+    puts Time.zone.today
+    self.end_date = Time.zone.today if self.end_date.nil?
     puts self.end_date
     puts "-"*500
   end

0278a52ce3d94cd394170518beb8328bca11f1b2
jplethier <jplethier@gmail.com>
2012-05-14 22:17:07 -0300

    corrigindo groupon share

--- app/stores/groupon.rb
+++ app/stores/groupon.rb
@@ -8,12 +8,12 @@ class Groupon
     page = Share.open_page(link)
 
     deal = Deal.new :link => link
-    deal.title = page.at_css("#contentDealTitle").try(:text).try(:strip)[0,255]
-    deal.price_mask = page.at_css(".noWrap").try(:text).try(:strip)[3..-1].try(:strip)
+    deal.title = page.at_css("#contentDealTitle").try(:text).try(:strip)[0,255] if page.at_css("#contentDealTitle")
+    deal.price_mask = page.at_css(".noWrap").try(:text).try(:strip)[3..-1].try(:strip) if page.at_css(".noWrap")
     #deal.real_price_mask = page.at_css(".regular").try(:text).try(:strip)[6..-1].try(:strip)
-    deal.description = page.at_css(".contentDealDescriptionFacts").try(:text).try(:strip)[0,1200]
+    deal.description = page.at_css(".contentDealDescriptionFacts").try(:text).try(:strip)[0,1200] if page.at_css(".contentDealDescriptionFacts")
     deal.company = "Groupon"
-    deal.image_url = page.at_css(".nobg").at_xpath(".//img")[:src].try(:strip)
+    deal.image_url = page.at_css(".nobg").at_xpath(".//img")[:src].try(:strip) if page.at_css(".nobg")
     #TODO: O método consegue setar city_id da oferta, mas não consegue exibir corretamente já na tela de cadastro de nova oferta
     #deal.city = City.find_by_name(page.at_css("#headerCityButton").try(:text).try(:strip))
     #if deal.city

39e5eb3b0fa45c1f2fb8da5b152d4074c82f250b
jplethier <jplethier@gmail.com>
2012-05-15 21:41:24 -0300

    corrigindo bug do submarino

--- app/stores/submarino.rb
+++ app/stores/submarino.rb
@@ -4,14 +4,12 @@ class Submarino
     page = Share.open_page(link)
 
     deal = Deal.new :link => link
-    deal.title = page.at_css("title").try(:text).try(:strip)[0,255]
-    if page.at_css(".for").try(:text) && page.at_css(".ficheTechnique").try(:text)
-      deal.price_mask = page.at_css(".for").try(:text).try(:strip)[7..-1].try(:strip)
-      deal.real_price_mask = page.at_css(".from").try(:text).try(:strip)[6..-1].try(:strip)
-      deal.description = page.at_css(".ficheTechnique").try(:text).try(:strip)[0,1200]
-      #deal.category = CATEGORIES[page.at_css(".selected").try(:text).try(:strip)]
-      deal.image_url = page.at_css("#baseImg")[:src].try(:strip)
-    end
+    deal.title = page.at_css("title").try(:text).try(:strip)[0,255] if page.at_css("title")
+    deal.price_mask = page.at_css(".for").try(:text).try(:strip)[7..-1].try(:strip) if page.at_css(".for")
+    deal.real_price_mask = page.at_css(".from").try(:text).try(:strip)[6..-1].try(:strip) if page.at_css(".from")
+    deal.description = page.at_css(".ficheTechnique").try(:text).try(:strip)[0,1200] if page.at_css(".ficheTechnique")
+    #deal.category = CATEGORIES[page.at_css(".selected").try(:text).try(:strip)]
+    deal.image_url = page.at_css("#baseImg")[:src].try(:strip) if page.at_css("#baseImg")
     deal.company = "Submarino"
     deal.kind = Deal::KIND_OFFER
 

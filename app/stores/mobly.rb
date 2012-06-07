65e12f311bc2b4c2841a3042e942bcd8871a1aac
jplethier <jplethier@gmail.com>
2012-06-06 21:11:46 -0300

    continuando refatoraçao para passar as views para haml consertando categoria das ofertas da mobly

--- app/stores/mobly.rb
+++ app/stores/mobly.rb
@@ -8,7 +8,7 @@ class Mobly
     deal.price_mask = page.at_css("#special_price_box").try(:text).split(" ")[1] if page.at_css("#special_price_box") && page.at_css("#special_price_box").try(:text)
     deal.real_price_mask = page.at_css("#price_box").try(:text).split(" ")[1] if page.at_css("#price_box") && page.at_css("#price_box").try(:text)
     deal.description = page.at_css(".prd-descriptionText").try(:text).try(:strip)[0,255] if page.at_css(".prd-descriptionText")
-    deal.category = Deal::CATEGORY_KIDS
+    deal.category = Deal::CATEGORY_OTHER
     deal.image_url = page.at_css("#prdImage")["src"].try(:strip) if page.at_css("#prdImage")
     deal.company = "Mobly"
     deal.kind = Deal::KIND_OFFER

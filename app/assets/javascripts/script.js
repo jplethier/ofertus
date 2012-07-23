4482eff53cb358eb29ea44b7ddcd2cf96c4142a3
jplethier <jplethier@gmail.com>
2012-07-23 20:42:22 -0300

    copiando arquivos de imagens, javascripts e fonts para o projeto

---
+++ app/assets/javascripts/script.js
@@ -0,0 +1,49 @@
+/* Ofertus
+ * @author Phillipe klepper - klepperss@gmail.com
+ * @version 1.0
+ * @date 15/07/2012
+ * @copyright (c) 2011 Phillipe klepper - http://www.klepper.com.br
+ */
+
+$('.bt-compartilhe-off').click(function() {
+	$('#box-compartilhe').slideToggle();
+});
+
+
+
+
+$("#categorias-home a").hover(function(){
+    $(this).parent().find("span").stop().animate({"opacity": 1});
+},function(){
+    $(this).parent().find("span").stop().animate({"opacity": 0});
+});
+
+
+
+$('.slide:first').show().parent().find(".bt-info").addClass("bt-mais-slide");
+$('.bt-info').click(function() {
+  $(this).parent().find('.slide').slideToggle('slow', function() {
+    $(this).parent().find(".bt-info").toggleClass('bt-mais-slide');
+  });
+});
+
+
+/* reset default value */
+jQuery.fn.resetDefaultValue = function() {
+	function _clearDefaultValue() {
+		var _$ = $(this);
+		if (_$.val() == this.defaultValue) {
+			_$.val('');
+		}
+	};
+	
+	function _resetDefaultValue() {
+		var _$ = $(this);
+		if (_$.val() == '') {
+			_$.val(this.defaultValue);
+		}
+	};
+	return this.click(_clearDefaultValue).focus(_clearDefaultValue).blur(_resetDefaultValue);
+}
+
+$('.resetValue').resetDefaultValue();

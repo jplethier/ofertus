eb437d792915384faea496d8b152d87d45b2d8a4
jplethier <jplethier@gmail.com>
2012-08-14 23:07:15 -0300

    incluindo mais testes da página de lista de usuários

--- spec/requests/users_page_spec.rb
+++ spec/requests/users_page_spec.rb
@@ -35,10 +35,13 @@ describe "Search Users Page" do
     end
 
     it 'should show the number of shared deals' do
-      within "#col-left .list-usuarios ul" do
+      within "#col-left" do
         deal = FactoryGirl.create(:deal, :user => user)
         visit users_path
-        should have_css('li', :text => '1 oferta compartilhada!')
+        have_css('li', :text => '1 oferta compartilhada!')
+        another_deal = FactoryGirl.create(:deal, :user => user)
+        visit users_path
+        have_css('li', :text => '2 ofertas compartilhadas!')
       end
     end
 
@@ -51,7 +54,49 @@ describe "Search Users Page" do
         another_followed = FactoryGirl.create(:user)
         user.follow!(another_followed)
         visit users_path
-        should have_css('li', :text => 'Seguindo 2 usuários!')
+        should have_css('li', :text => 'Seguindo 09 usuários!')
+      end
+    end
+  end
+
+  describe 'side search' do
+    it 'should show search title' do
+      within '#col-right #busca-home' do
+        should have_content('Opções de Busca')
+      end
+    end
+
+    it 'should show search form' do
+      within '#col-right #busca-home' do
+        should have_css("input#search")
+        should have_button("search_button")
+      end
+    end
+
+    it 'search by user and find nothing' do
+      user.name = 'Nome'
+      user.save
+      within "#col-right" do
+        fill_in 'search', :with => 'Nome Diferente'
+        click_button 'search_button'
+      end
+      should have_content('Usuários')
+      within "#col-left .list-usuarios" do
+        should_not have_css('a.user_picture')
+      end
+    end
+
+    it 'search by user and find one user' do
+      user.name = 'Nome'
+      user.save
+      within "#col-right" do
+        fill_in 'search', :with => 'Nome'
+        click_button 'search_button'
+      end
+      should have_content('Usuários')
+      within "#col-left .list-usuarios" do
+        should have_css('a.user_picture')
+        should have_link(user.name)
       end
     end
   end

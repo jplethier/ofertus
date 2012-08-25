50e04f1646d513d4869a4879143ddd26ab3bd63c
jplethier <jplethier@gmail.com>
2012-08-25 17:24:54 -0300

    excluindo gem resque que nao estava sendo utilizada

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -1,6 +1,6 @@
-ENV["REDISTOGO_URL"] ||= "redis://redistogo:dbfc9fbbae2b3a319bc56c54137f7220@gar.redistogo.com:9200//"
+# ENV["REDISTOGO_URL"] ||= "redis://redistogo:dbfc9fbbae2b3a319bc56c54137f7220@gar.redistogo.com:9200//"
 
-uri = URI.parse(ENV["REDISTOGO_URL"])
-Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true)
+# uri = URI.parse(ENV["REDISTOGO_URL"])
+# Resque.redis = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password, :thread_safe => true)
 
-Dir["/app/app/jobs/*.rb"].each { |file| require file }
+# Dir["/app/app/jobs/*.rb"].each { |file| require file }

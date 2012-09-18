e5d6bde2824983371d0de4568d12742cb0f9d962
jplethier <jplethier@gmail.com>
2012-09-17 22:50:05 -0300

    corrigindo em todos os lugares para usar Ofertus ao inves de OfertuS

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,9 +15,9 @@ module ApplicationHelper
 
   def title
     if not @title.nil?
-      @title + " | Ofertus - Compartilhe ofertas e ganhe com isso"
+      @title + " | Ofertus - Compartilhe Ofertas"
     else
-      "Ofertus - Compartilhe ofertas e ganhe com isso"
+      "Ofertus - Compartilhe Ofertas e ganhe com isso"
     end
   end
 

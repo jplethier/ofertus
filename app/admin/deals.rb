f0dc32c0098117f407b55101759bdf1aafc65550
jplethier <jplethier@gmail.com>
2012-09-22 19:30:56 -0300

    começando a usar o activeadmin

---
+++ app/admin/deals.rb
@@ -0,0 +1,3 @@
+ActiveAdmin.register Deal do
+  
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -29,4 +29,8 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+
+  config.before(:all) do
+    I18n.locale = 'pt-BR'
+  end
 end

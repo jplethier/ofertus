c31c82747996cc98744a7417a77c6e8fd1eb53b3
jplethier <jplethier@gmail.com>
2012-09-22 17:15:07 -0300

    retirando codigo que nao esta sendo mais usado do application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -29,10 +29,6 @@ module ApplicationHelper
     end
   end
 
-  def today?
-    controller_name == 'deals' && action_name ==  'today'
-  end
-
   # TODO: resolver via js
   # def link_to_active(*args, &block)
   #   if block_given?

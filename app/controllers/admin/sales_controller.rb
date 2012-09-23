f80e5f52236684a888045836b549082710ba1e54
jplethier <jplethier@gmail.com>
2012-09-23 13:53:12 -0300

    asdasdasdasd

---
+++ app/controllers/admin/sales_controller.rb
@@ -0,0 +1,18 @@
+class Admin::SalesController < Admin::ApplicationController
+
+  def index
+    @sales = Sale.all
+  end
+
+  def new
+    
+  end
+
+  def edit
+    
+  end
+
+  def show
+    
+  end
+end

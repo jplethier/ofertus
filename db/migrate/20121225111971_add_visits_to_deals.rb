3223a86ef6bea74de11945dd278572938b71b05d
jplethier <jplethier@gmail.com>
2012-10-15 23:35:54 -0300

    setando valor default(0) para contador de visitas de ofertas

--- db/migrate/20121225111971_add_visits_to_deals.rb
+++ db/migrate/20121225111971_add_visits_to_deals.rb
@@ -1,5 +1,5 @@
 class AddVisitsToDeals < ActiveRecord::Migration
   def change
-    add_column :deals, :visits, :integer
+    add_column :deals, :visits, :integer, :default => 0
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121225111970) do
+ActiveRecord::Schema.define(:version => 20121225111971) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -115,7 +115,7 @@ ActiveRecord::Schema.define(:version => 20121225111970) do
     t.string   "image_url"
     t.string   "slug"
     t.boolean  "ofertus_top"
-    t.integer  "visits"
+    t.integer  "visits",                                    :default => 0
   end
 
   add_index "deals", ["city_id"], :name => "index_deals_on_city_id"

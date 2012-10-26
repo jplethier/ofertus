00e8e5d556a3619a7512362cf7c135f35db73ef4
jplethier <jplethier@gmail.com>
2012-10-26 13:37:48 -0200

    paginas de seguidos e seguidores pronta

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,9 +26,11 @@ class UsersController < ApplicationController
   end
 
   def followers
+    @followers = @user.followers.paginate(page: params[:page], per_page: 12)
   end
 
   def following
+    @following = @user.followers.paginate(page: params[:page], per_page: 12)
   end
 
   #Lembrar de dar Reset Ability (CanCan)

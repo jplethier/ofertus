044c522f2d4c27c1bd88e1d313fa8e423ec08430
jplethier <jplethier@gmail.com>
2012-11-17 16:17:26 -0200

    corrigindo bot da toymania

--- app/stores/toy_mania.rb
+++ app/stores/toy_mania.rb
@@ -7,7 +7,7 @@ class ToyMania
     unless page.nil?
       deal.link = link
       deal.title = page.at_css(".productName").try(:text).try(:strip)[0,255] if page.at_css(".productName")
-      deal.price_mask = page.at_css(".valor-por").try(:text).try(:strip).split(" ")[2].try(:strip) if page.at_css(".valor-por") && page.at_css(".valor-por").try(:text)
+      deal.price_mask = page.at_css(".valor-por .skuBestPrice").try(:text).split(' ')[1] if page.at_css(".valor-por .skuBestPrice").try(:text)
       deal.real_price_mask = page.at_css(".valor-de").try(:text).try(:strip).split(" ")[2].try(:strip) if page.at_css(".valor-de") && page.at_css(".valor-de").try(:text)
       deal.description = page.at_css(".productName").try(:text).try(:strip)[0,255] if page.at_css(".productName")
       deal.image_url = "http://www.toymania.com.br/" + page.at_css("#image-main")["src"].try(:strip) if page.at_css("#image-main")

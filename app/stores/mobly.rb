7439a4ab158ddac51cffa26779b8600dbf6489c8
jplethier <jplethier@gmail.com>
2012-11-17 16:17:10 -0200

    corrigindo o bot da mobly

--- app/stores/mobly.rb
+++ app/stores/mobly.rb
@@ -7,12 +7,15 @@ class Mobly
     unless page.nil?
       deal.link = link
       deal.title = page.at_css(".prd-title").try(:text).try(:strip)[0,255] if page.at_css(".prd-title")
-      deal.price_mask = page.at_css("#special_price_box").try(:text).split(" ")[1] if page.at_css("#special_price_box") && page.at_css("#special_price_box").try(:text)
-      deal.real_price_mask = page.at_css("#price_box").try(:text).split(" ")[1] if page.at_css("#price_box") && page.at_css("#price_box").try(:text)
+      
+      deal.price_mask = page.at_css("#special_price_box").try(:text).try(:strip) if page.at_css("#special_price_box") && page.at_css("#special_price_box")
+
+      deal.real_price_mask = page.at_css('#price_box').try(:text).try(:strip) if page.at_css('#price_box')
+
       deal.description = page.at_css(".prd-descriptionText").try(:text).try(:strip)[0,255] if page.at_css(".prd-descriptionText")
       deal.image_url = page.at_css("#prdImage")["src"].try(:strip) if page.at_css("#prdImage")
     end
-    deal.category = Deal::CATEGORY_OTHER
+    deal.category = Deal::CATEGORY_HOME_AND_DECORATION
     deal.company = "Mobly"
     
     deal

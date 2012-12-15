d1264ef23faac9a798debe1c8e425507223e8eeb
jplethier <jplethier@gmail.com>
2012-12-15 13:25:40 -0200

    bot do submarino funcionando, só não preenche a categoria sozinho

--- app/stores/submarino.rb
+++ app/stores/submarino.rb
@@ -1,17 +1,47 @@
+#coding: utf-8
 class Submarino
-  
+  CATEGORIES = {
+    'Alimentos e Bebidas' => Deal::CATEGORY_OTHER,
+    'Automotivo' => Deal::CATEGORY_CAR,
+    'Bebês' => Deal::CATEGORY_OTHER,
+    'Beleza e Saúde' => Deal::CATEGORY_BEAUTY_AND_HEALTH,
+    'Brinquedos' => Deal::CATEGORY_OTHER,
+    'Cama, Mesa e Banho' => Deal::CATEGORY_HOME_AND_APPLIANCE,
+    'Cine e Foto' => Deal::CATEGORY_OTHER,
+    'DVDs e Blu-Ray' => Deal::CATEGORY_CULTURE,
+    'Eletrônicos' => Deal::CATEGORY_COMPUTER,
+    'Eletrodomésticos' => Deal::CATEGORY_HOME_AND_APPLIANCE,
+    'Eletrônicos' => Deal::CATEGORY_COMPUTER,
+    'Eletroportáteis' => Deal::CATEGORY_HOME_AND_APPLIANCE,
+    'Esporte e Lazer' => Deal::CATEGORY_ENTERTAINMENT,
+    'Ferramentas' => Deal::CATEGORY_OTHER,
+    'Flores' => Deal::CATEGORY_OTHER,
+    'Games' => Deal::CATEGORY_OTHER,
+    'Informática' => Deal::CATEGORY_COMPUTER,
+    'Livros' => Deal::CATEGORY_CULTURE,
+    'Malas & Acessórios' => Deal::CATEGORY_OTHER,
+    'Moda' => Deal::CATEGORY_CLOTHES,
+    'Móveis' => Deal::CATEGORY_HOME_AND_APPLIANCE,
+    'Papelaria' => Deal::CATEGORY_OTHER,
+    'Perfumaria' => Deal::CATEGORY_BEAUTY_AND_HEALTH,
+    'Relógios' => Deal::CATEGORY_CLOTHES,
+    'Tablets' => Deal::CATEGORY_COMPUTER,
+    'Telefones & Celulares' => Deal::CATEGORY_COMPUTER,
+    'Utilidades Domésticas' => Deal::CATEGORY_HOME_AND_APPLIANCE
+  }
+
   def self.fill_deal_fields(link)
     page = Share.open_page(link)
 
     deal = Deal.new
+    deal.link = link
     unless page.nil?
-      deal.link = link
       deal.title = page.at_css("title").try(:text).try(:strip)[0,255] if page.at_css("title")
-      deal.price_mask = page.at_css(".for").try(:text).try(:strip)[7..-1].try(:strip) if page.at_css(".for")
-      deal.real_price_mask = page.at_css(".from").try(:text).try(:strip)[6..-1].try(:strip) if page.at_css(".from")
-      deal.description = page.at_css(".ficheTechnique").to_s.truncate(4000) if page.at_css(".ficheTechnique")
-      #deal.category = CATEGORIES[page.at_css(".selected").try(:text).try(:strip)]
-      deal.image_url = page.at_css("#baseImg")[:src].try(:strip) if page.at_css("#baseImg")
+      deal.price_mask = page.at_css(".sale .amount").try(:text).try(:strip) if page.at_css(".sale .amount") && page.at_css(".sale .amount").try(:text)
+      deal.real_price_mask = page.at_css(".regular .amount").try(:text).try(:strip)[2..-1].try(:strip) if page.at_css(".regular .amount") && page.at_css(".regular .amount").try(:text)
+      deal.description = page.at_css("#productDescription").to_s.truncate(4000) if page.at_css("#productDescription")
+      deal.category = CATEGORIES[page.at_css(".selected").try(:text).try(:strip)]
+      deal.image_url = page.at_css("#imgProduto")[:src].try(:strip) if page.at_css("#imgProduto")
     end
     deal.company = "Submarino"
     

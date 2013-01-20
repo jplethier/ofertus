ff4b8b66ec22242eac1a26c7b509571b693fca44
jplethier <jplethier@gmail.com>
2013-01-20 20:54:24 -0200

    fix the unique link validator

--- app/validators/unique_link_validator.rb
+++ app/validators/unique_link_validator.rb
@@ -1,6 +1,6 @@
 class UniqueLinkValidator < ActiveModel::EachValidator
   def validate_each(record,attribute,value)
-    if (!Deal.active.where(link:value).blank?)
+    if (!Deal.active.where('link = ? and id != ? ', value, record.id).blank?)
         record.errors.add(attribute,:unique_link,options)
     end
   end

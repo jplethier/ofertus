2f4536e880f6beae98f55fad311bc799721e78a0
jplethier <jplethier@gmail.com>
2013-01-31 23:34:02 -0200

    acrescentando filtro por desconto na header, falta uma parte do css do filtro ainda

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -137,6 +137,7 @@ class DealsController < AuthorizedController
     @deals = @deals.search(params[:search]) unless params[:search].blank?
     @deals = @deals.by_cities(params[:search_city]) unless params[:search_city].blank?
     @deals = @deals.by_price_range(params[:price_range].split('-')[0].gsub('R$','').to_i, params[:price_range].split('-')[1].gsub('R$','').to_i) if params[:price_range]
+    @deals = @deals.by_discount_range(params[:discount_range].split('-')[0].to_i, params[:discount_range].split('-')[1].to_i) if params[:discount_range]
     @deals = @deals.send(params[:search_order]) if params[:search_order]
     @deals = @deals.active
   end

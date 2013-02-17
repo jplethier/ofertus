e0811d59320d64223d9681cc1ac2464070260707
João Paulo Lethier <jplethier@gmail.com>
2013-02-17 01:08:51 -0300

    userbox
    acoes de seguir e deixar de seguir na box de usuario funcionando

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -5,7 +5,7 @@ function follow(user_username, link){
       if(data.status == 'erro'){
         $('#flashes').append('<div class="alert error" id="flash_notice"><p>' + data.message + '</p></div>')
       }else{
-        $('#'+link).attr('onclick',"unfollow('"+user_username+"','"+link+"')").addClass('unfollow')
+        $('.'+link).attr('onclick',"unfollow('"+user_username+"','"+link+"')").addClass('unfollow')
         $('#flashes').append('<div class="alert success" id="flash_notice"><p>' + data.message + '</p></div>')
       }
       center_flash();
@@ -15,14 +15,14 @@ function follow(user_username, link){
   })
 }
 
-function unfollow(user_id, link){
+function unfollow(user_username, link){
   $.ajax({
     url: 'users/'+user_username+'/unfollow',
     success: function(data){
       if(data.status == 'erro'){
         $('#flashes').append('<div class="alert error" id="flash_notice"><p>' + data.message + '</p></div>')
       }else{
-        $('#'+link).attr('onclick',"follow('"+user_username+"','"+link+"')").removeClass('unfollow')
+        $('.'+link).attr('onclick',"follow('"+user_username+"','"+link+"')").removeClass('unfollow')
         $('#flashes').append('<div class="alert success" id="flash_notice"><p>' + data.message + '</p></div>')
       }
       center_flash();

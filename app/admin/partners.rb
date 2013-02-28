c631f68c44d4d016a85b3638ed7790416bc8f114
João Paulo Lethier <jplethier@gmail.com>
2013-02-27 21:33:44 -0300

    admin
    melhorando a exibição de parceiros no admin

--- app/admin/partners.rb
+++ app/admin/partners.rb
@@ -1,7 +1,20 @@
+#coding: utf-8
 ActiveAdmin.register Partner do
 
   scope :active
   scope :inactive
   scope :promotion
 
+  index do
+    column 'Logo' do |partner|
+      image_tag(partner.mini_image_url)
+    end
+    column 'Nome', :name
+    column 'Código', :code
+    column 'Ativo?', :active
+    column 'URL' do |partner|
+      link_to partner.url, partner.url, target: '_blank'
+    end
+    default_actions
+  end
 end

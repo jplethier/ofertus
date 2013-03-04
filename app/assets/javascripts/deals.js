d7c78b1183263bb8d3c07ceaac7c4b8a36cf7cb6
João Paulo Lethier <jplethier@gmail.com>
2013-03-03 21:13:23 -0300

    endless page
    carregando novas ofertas via js quando o usuario chega no final da
    pagina

--- app/assets/javascripts/deals.js
+++ app/assets/javascripts/deals.js
@@ -91,3 +91,13 @@ function unreport_deal(deal_id){
     }
   });
 }
+
+$(document).ready(function() {
+  $(window).scroll(function() {
+    url = $('.pagination .next_page').attr('href')
+    if( url &&  $(window).scrollTop() > $(document).height() - $(window).height() - 50){
+      $('.pagination').html('<p>Carregando mais ofertas...</p>')
+      $.getScript(url)
+    }
+  })
+});

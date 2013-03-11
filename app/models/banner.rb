f2d6d34acb5a9d77d32e7aa0780b3a764e7318a1
João Paulo Lethier <jplethier@gmail.com>
2013-03-11 00:24:43 -0300

    slider funcionando
    banners alternando entre eles automaticamente

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -2,8 +2,12 @@ class Banner < ActiveRecord::Base
   attr_accessible :active, :alt, :image, :target, :title, :url
 
   has_attached_file :image,
-                    styles: { medium: "300x300>", thumb: "100x100>" },
+                    styles: { medium: "300x300>", thumb: "110x90>", banner: "520x280>" },
                     default_url: "/images/banners/:style/missing.png",
                     url: "banners/:id_partition/:style.:extension",
                     path: "banners/:id_partition/:style.:extension"
+
+  scope :active, lambda { where(active: true).order('created_at desc').limit(3) }
+  scope :all_active, lambda { where(active: true) }
+  scope :recent, lambda { order('created_at desc') }
 end

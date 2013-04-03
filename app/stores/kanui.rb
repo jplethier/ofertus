f17310c5b5ee992735bc82b9c6d76cba15e24eb1
João Paulo Lethier <jplethier@gmail.com>
2013-04-02 21:46:14 -0300

    corrigindo bot da kanui

--- app/stores/kanui.rb
+++ app/stores/kanui.rb
@@ -7,12 +7,12 @@ class Kanui
     unless page.nil?
       deal.link = link
 
-      deal.title = page.at_css("h1.prd-title").try(:text).try(:strip)[0,255] if page.at_css("h1.prd-title").try(:text)
-      deal.real_price_mask = page.at_css("span#price_box").try(:text).try(:strip).split(' ')[1] if page.at_css("span#price_box").try(:text)
-      deal.price_mask = page.at_css("span#special_price_box").try(:text).try(:strip).split(' ')[1] if page.at_css("span#special_price_box").try(:text)
-      deal.description = page.at_css("#productDetails").try(:text).truncate(4000) if page.at_css("#productDetails")
+      deal.title = page.at_css(".prd-detail-brand").try(:text).try(:strip)[0,255] if page.at_css(".prd-detail-brand").try(:text)
+      deal.real_price_mask = page.at_css(".price-from").try(:text).try(:strip).split(' ')[2] if page.at_css(".price-from").try(:text)
+      deal.price_mask = page.at_css("span#product_special_price").try(:text).try(:strip) if page.at_css("span#product_special_price").try(:text)
+      deal.description = page.at_css("#details .prd-details").try(:text).truncate(4000) if page.at_css("#details .prd-details").try(:text)
       deal.category = Deal::CATEGORY_CLOTHES
-      deal.image_url = page.at_css("#prdZoomBox span img")[:src].try(:strip) if page.at_css("#prdZoomBox span img") && page.at_css("#prdZoomBox span img")[:src]
+      deal.image_url = page.at_css("#img-target")[:src].try(:strip) if page.at_css("#img-target") && page.at_css("#img-target")[:src]
     end
     deal.company = "Kanui"
 

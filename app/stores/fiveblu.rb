2fadb63c80d7bb14ad33ea089e1ab36948b5a763
João Paulo Lethier <jplethier@gmail.com>
2013-04-20 16:32:32 -0300

    bot da fiveblu funcionando

--- app/stores/fiveblu.rb
+++ app/stores/fiveblu.rb
@@ -5,18 +5,18 @@ class Fiveblu
     page = Share.open_page(link)
 
     unless page.nil?
-      deal.link = link
 
-      deal.title = page.at_css(".prd-detail-brand").try(:text).try(:strip)[0,255] if page.at_css(".prd-detail-brand").try(:text)
-      deal.real_price_mask = page.at_css(".price-from").try(:text).try(:strip).split(' ')[2] if page.at_css(".price-from").try(:text)
-      deal.price_mask = page.at_css("span#product_special_price").try(:text).try(:strip) if page.at_css("span#product_special_price").try(:text)
-      deal.description = page.at_css("#details .prd-details").try(:text).truncate(4000) if page.at_css("#details .prd-details").try(:text)
-      deal.category = Deal::CATEGORY_CLOTHES
-      deal.image_url = page.at_css("#img-target")[:src].try(:strip) if page.at_css("#img-target") && page.at_css("#img-target")[:src]
+      deal.title = page.at_css(".product-name").try(:text).try(:strip)[0,255] if page.at_css(".product-name").try(:text)
+      deal.real_price_mask = page.at_css(".price-up").try(:text).try(:strip).split[1] if page.at_css(".price-up").try(:text)
+      deal.price_mask = page.at_css(".price-to").try(:text).try(:strip).split[1] if page.at_css(".price-to").try(:text)
+      deal.description = page.at_css(".productdescription").try(:text).truncate(4000) if page.at_css(".productdescription").try(:text)
+      deal.image_url = page.at_css("#product-image-image")[:src] if page.at_css("#product-image-image")
     end
-    deal.company = "Kanui"
+    deal.category = Deal::CATEGORY_CLOTHES
+    deal.link = link
+    deal.company = "Fiveblu"
 
-    partner = Partner.find_by_name('Kanui')
+    partner = Partner.find_by_name('Fiveblu')
     unless partner.blank?
       deal.partner = partner
     end
